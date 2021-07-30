4fb3587ab2b0efc33abc3fa843289677eb985e5e
mathew1357 <58839700+mathew1357@users.noreply.github.com>
2021-07-30 09:05:19 +0900

    Update xtra.sh

--- xtra.sh
+++ xtra.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Project Seve
 clear
+function trap(){
 read -p "New Replace in Status 101: " bake
 $bowl = sed '18q;d' /usr/local/bin/projectseve
 $bowls = echo "RESPONSE = 'HTTP/1.1 101 <font color="red">$bake</font>\r\n\r\nContent-Length: 104857600000\r\n\r\n'"
@@ -9,3 +10,12 @@ systemctl restart projectseve
 clear
 sleep 2
 echo "done"
+}
+function runstats(){
+read -p "New Replace in Status 101: " bake
+$substitute = "RESPONSE = 'HTTP/1.1 101 <font color='red'>$bake</font>\r\n\r\nContent-Length: 104857600000\r\n\r\n'"
+sed '/104857600000/d' /usr/local/bin/projectseve
+sed -i "18|.*|$substitute|"  
+echo "done"
+}
+runstats
